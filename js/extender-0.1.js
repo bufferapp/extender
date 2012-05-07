3280875c3c95ea29da6b477c082b0e00f06a78b8
Tom Ashworth <tom@phuu.net>
2012-05-07 21:38:18 +0100

    Added: jQuery, test html page & minified version.

--- js/extender-0.1.js
+++ js/extender-0.1.js
@@ -1,3 +1,4 @@
+
 /**
  * Extender
  * 
@@ -5,24 +6,22 @@
  * 
  * Design decisions:
  *   - Parameters to all functions are passed as hashes (so parameter ordering is not a problem)
- * 
- * Possible APIs:
- * 
- * 
-
-  xt({port: ... })
-  xt.port({port: ... })
-
-
+ *   
+ * Dependencies:
+ *   - jQuery. It's good code, everybody knows the API and it's very good cross browser.
  *
  * TODO:
  *   - Data wrapping
  *   - DOM building methods
  *   - (reliable) CSS insertion
  */
+
+// Imports
+// @codekit-prepend "lib/jquery-1.7.2.min.js";
+
 if( ! xt || typeof xt === "object") {
 
-var xt = (function (window, document, self, chrome, safari, undefined) {
+var xt = (function (window, document, undefined) {
 
   var ENV = {
     chrome: 1,
@@ -41,12 +40,24 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
     else console.log.apply(console, error);
   };
 
+  Extender.config = {
+    data: {
+      endpoint: {
+        http: '',
+        https: ''
+      }
+    }
+  };
+
   // Detect environment
   if( chrome ) Extender.env = ENV.chrome;
   else if( safari ) Extender.env = ENV.safari;
   else if( self && self.extension ) Extender.env = ENV.firefox;
   else {
-    Extender.error("Environment not known.", true); // Make this object
+    Extender.error({
+      err: "Environment not known.",
+      fatal: true
+    });
   }
 
   Extender.isChrome = (function () { return (Extender.env === ENV.chrome); }());
@@ -66,14 +77,14 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
       , contentScript = false
       , sub = {};
 
-    if( ! ob.name ) ob.name = "embed";
+    if( ! ob.name ) { ob.name = "embed"; }
     
     // Store raw port & do browser specific setup
     if( Extender.isChrome ) {
       port = chrome.extension.connect({name: ob.name})
       // Subscribe to events
       port.onMessage.addListener(function (data) {
-        if( !sub[data.type] ) return;
+        if( !sub[data.type] ) { return; }
         // Distribute event to all subscribers
         var i, length = sub[data.type].length;
         for( i=0; i < length; i++ ) {
@@ -83,7 +94,7 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
     }
     if( Extender.isSafari ) {
       port = safari.self;
-      if( port.tab ) contentScript = true;
+      if( port.tab ) { contentScript = true; }
     }
     if( Extender.isFirefox ) {
       port = self.port;
@@ -92,12 +103,12 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
     return {
       on: function (type, callback) {
         if( Extender.isChrome ) {
-          if( !sub[type] ) sub[type] = [];
+          if( !sub[type] ) { sub[type] = []; }
           sub[type].push(cb);
         }
         if( Extender.isSafari ) {
           return port.addEventListener("message", function (ev) {
-            if( ev.name === type ) callback(ev.message);
+            if( ev.name === type ) { callback(ev.message); }
           }, false);
         }
         if( Extender.isFirefox ) {
@@ -105,8 +116,7 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
         }
       },
       off: function (type) {
-        if( Extender.isChrome ) {
-        }
+        if( Extender.isChrome ) {}
         if( Extender.isSafari ) {
           port.removeEventListener(type);
         }
@@ -122,8 +132,8 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
           });
         }
         if( Extender.isSafari ) {
-          if( contentScript ) port.tab.dispatchMessage(type, payload);
-            else port.page.dispatchMessage(type, payload);
+          if( contentScript ) { port.tab.dispatchMessage(type, payload); }
+          else { port.page.dispatchMessage(type, payload); }
         }
         if( Extender.isFirefox ) {
           return port.emit(type, payload);
@@ -140,22 +150,62 @@ var xt = (function (window, document, self, chrome, safari, undefined) {
   };
 
   // Data wrapper
-  Extender.wrap.data = function () {};
+  Extender.wrap.data = function (ob) {
+
+    var file;
+
+    if( ! ob.file ) { return Extender.error("No data file specified."); }
+
+    file = ob.file;
+
+    if( Extender.isChrome ) {
+      return chrome.extension.getURL(file);
+    }
+    if( Extender.isSafari ) {
+      return (document.location.protocol === 'http:' ? config.endpoint.http : config.endpoint.https) + file;
+    }
+    if( Extender.isFirefox ) {
+      return (document.location.protocol === "http:" ? Extender.config.data.endpoint.http : Extender.config.data.endpoint.http) + file;
+    }
+
+  };
   
   // API
+  
+  // Config
+  // Usage:
+  //    xt({ ... });
 
   // Port
+  // Usage:
+  //    xt.port({name: 'myPort'});
+  //    xt.port.on('message', function (data) { ... });
+  //    xt.port.emit('message', { ... });
   Extender.port = function (ob) {
     return Extender.wrap.port(ob);
   };
-  Extender.data = function (ob) {
-    return Extender.wrap.data(ob);
+
+  // Data
+  // Usage:
+  //    xt.data.get({file: ' ... '});
+  //    xt.data.get({file: [ ... ]});
+  Extender.data = {
+    get: function (ob) {
+      return Extender.wrap.data(ob);
+    }
+  }
+
+  // 'ere we go.
+  console.log(Extender);
+  return function (config) {
+
+    // recursively merge supplied config & defaults
+    $.extend(true, Extender.config, config);
+
+    return Extender;
+
   };
 
+}(window, document));
 
-  console.log(Extender);
-  return Extender;
-
-}(window, document, self, chrome, safari));
-
-} // Only run this if xt does not exists
+} // Only run this if xt does not exist
